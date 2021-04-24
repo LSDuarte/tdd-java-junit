3411a2015d38971abc4d50de1432b6e055e55a08
Larissa Duarte <larissa.duarte@datainfo.inf.br>
2021-04-24 16:03:20 -0400

    Lidando com exceptions em testes

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.junit.jupiter.api.Assertions.fail;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -14,8 +16,16 @@ class BonusServiceTest {
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Teste 1", LocalDate.now(), new BigDecimal("25000")));
-		assertEquals(new BigDecimal("0.00"), bonus);
+		assertThrows(IllegalArgumentException.class,
+					() -> service.calcularBonus(new Funcionario("Teste 1", LocalDate.now(), new BigDecimal("25000"))));
+
+	/*	try {
+			service.calcularBonus(new Funcionario("Teste 1", LocalDate.now(), new BigDecimal("25000")));
+			fail("Não deu a exception!");
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			assertEquals("Funcionário com salário maior do que o previsto.", e.getMessage());
+		}*/
 	}
 	
 	@Test
